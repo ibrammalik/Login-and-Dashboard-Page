ff01b7b7d7a8ffcfdc75992f444b0fae4c402a42
ibrammalik <malikmaul1173@gmail.com>
2022-08-14 20:04:21 +0700

    Update
    + Fix Bug
    -Now, when you make multiple account, will note overwrite previous accout
    
    + Add Feature
    -Now, You can Upload your photo profile
    
    + Update UI

--- script.js
+++ script.js
@@ -1,145 +1,223 @@
-const masukRadio = document.getElementById("btnradio1")
-const daftarRadio = document.getElementById("btnradio2")
-const username = document.getElementById("username")
-const password = document.getElementById("password")
-const signInButton = document.getElementById("signInButton")
-const signUpButton = document.getElementById("signUpButton")
-const roleOptionContainer = document.getElementById("roleOptionContainer")
-const roleOption = document.getElementById("roleOption")
-const avatar1 = document.getElementById("avatar1")
-const avatar2 = document.getElementById("avatar2")
-const avatar3 = document.getElementById("avatar3")
-const navbarList = document.getElementById("navbarList")
-const roleStatus = document.getElementById("role")
-const dashboardPage = document.getElementById("dashboard-page")
-const loginPage = document.getElementById("login-page")
-const sidebar = document.getElementById("sidebar") 
+const masukRadio = document.getElementById("btnradio1");
+const daftarRadio = document.getElementById("btnradio2");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const signInButton = document.getElementById("signInButton");
+const signUpButton = document.getElementById("signUpButton");
+const roleOptionContainer = document.getElementById("roleOptionContainer");
+const roleOption = document.getElementById("roleOption");
+const avatar1 = document.getElementById("avatar1");
+const avatar2 = document.getElementById("avatar2");
+const avatar3 = document.getElementById("avatar3");
+const navbarList = document.getElementById("navbarList");
+const roleStatus = document.getElementById("role");
+const dashboardPage = document.getElementById("dashboard-page");
+const loginPage = document.getElementById("login-page");
+const loginBox = document.getElementById("login-box");
+const sidebar = document.getElementById("sidebar");
+const usernameProfile = document.getElementById("username-profile");
+const inputImages = document.getElementById("input-images");
+const photoProfile = document.getElementById("photo-profile");
 
 const pilihRole = () => {
-    if (roleOption.value == 'admin') {
-        avatar1.style.display = "none"
-        avatar2.style.display = "block"
-        avatar3.style.display = "none"
-    } else if (roleOption.value == 'lecturer') {
-        avatar1.style.display = "none"
-        avatar2.style.display = "none"
-        avatar3.style.display = "block"
-    } else if (roleOption.value == 'student') {
-        avatar1.style.display = "block"
-        avatar2.style.display = "none"
-        avatar3.style.display = "none"
-    }
-}
+  if (roleOption.value == "Admin") {
+    avatar1.style.display = "none";
+    avatar2.style.display = "block";
+    avatar3.style.display = "none";
+  } else if (roleOption.value == "Lecturer") {
+    avatar1.style.display = "none";
+    avatar2.style.display = "none";
+    avatar3.style.display = "block";
+  } else if (roleOption.value == "Student") {
+    avatar1.style.display = "block";
+    avatar2.style.display = "none";
+    avatar3.style.display = "none";
+  }
+};
 
 const pilih = () => {
-    if (masukRadio.checked) {
-        signInButton.style.display = "block"
-        signUpButton.style.display = "none"
-        roleOptionContainer.style.display = "none"
-    } else if (daftarRadio.checked) {
-        signInButton.style.display = "none"
-        signUpButton.style.display = "block"
-        roleOptionContainer.style.display = "block"
-    }
-}
+  if (masukRadio.checked) {
+    signInButton.style.display = "block";
+    signUpButton.style.display = "none";
+    roleOptionContainer.style.display = "none";
+  } else if (daftarRadio.checked) {
+    signInButton.style.display = "none";
+    signUpButton.style.display = "block";
+    roleOptionContainer.style.display = "block";
+  }
+};
 
 const allStorage = () => {
-    var values = [],
-        keys = Object.keys(localStorage),
-        i = keys.length;
-    while ( i-- ) {
-        values.push( localStorage.getItem(keys[i]) );
-    }
-    return values;
-}
-
-const storageValues = allStorage()
+  var values = [],
+    keys = Object.keys(localStorage),
+    i = keys.length;
+  while (i--) {
+    values.push(localStorage.getItem(keys[i]));
+  }
+  return values;
+};
 
 const search = (input) => {
-    for(i = 0; i < storageValues.length; i++) {
-        if (input == storageValues[i]) {
-            return input
-        }
+  const storageValues = allStorage();
+  for (i = 0; i < storageValues.length; i++) {
+    if (input == storageValues[i]) {
+      return input;
     }
-}
+  }
+};
 
-const storageKeys = [...Object.keys(localStorage)]
 const searchKeys = (values) => {
-    let i = 0
-    for (i; i < storageKeys.length; i++) {
-        if (localStorage.getItem(storageKeys[i]) == values) {
-            return storageKeys[i]
-        } 
+  const storageKeys = [...Object.keys(localStorage)];
+  let i = 0;
+  for (i; i < storageKeys.length; i++) {
+    if (localStorage.getItem(storageKeys[i]) == values) {
+      return storageKeys[i];
     }
-}
+  }
+};
 
 const makeUserNumber = () => {
-    let usernumber = Number(localStorage.getItem('usernumber'))
-    if (localStorage.getItem('usernumber') == null) {
-        localStorage.setItem('usernumber', '0')
-    } else if (localStorage.getItem('usernumber')) {
-        usernumber++
-        localStorage.setItem('usernumber', usernumber.toString())
-        console.log(usernumber)
-    }
-    return usernumber
-}
-const userNumber = makeUserNumber()
+  let usernumber = Number(localStorage.getItem("usernumber"));
+  if (localStorage.getItem("usernumber") == null) {
+    localStorage.setItem("usernumber", "0");
+  } else if (localStorage.getItem("usernumber")) {
+    usernumber++;
+    localStorage.setItem("usernumber", usernumber.toString());
+  }
+  return usernumber;
+};
 
 const signUp = () => {
-    if (roleOption.value == 0) {
-        window.alert ('pilih role nya dulu cuy')
-    } else if (username.value === "") {
-        window.alert("Harap masukan username dengan benar")
-    } else if (password.value === "") {
-        window.alert("Harap masukan password dengan benar")
-    } else if(username.value && password.value) {
-        localStorage.setItem(`username${userNumber}`, username.value)
-        localStorage.setItem(`password${userNumber}`, password.value)
-        localStorage.setItem(`role${userNumber}`, roleOption.value)
-        window.confirm("Kamu sudah terdaftar cuy silahkan login")
-        location.reload()
-    }
-}
+  const userNumber = makeUserNumber();
+  if (roleOption.value == 0) {
+    swal("Role Nya Di Pilih Dulu Cuy!!!");
+  } else if (username.value === "") {
+    swal("Harap masukan username dengan benar");
+  } else if (password.value === "") {
+    swal("Harap masukan password dengan benar");
+  } else if (username.value == search(username.value)) {
+    swal("Username sudah ada cuy silahkan ganti username");
+  } else if (username.value && password.value) {
+    localStorage.setItem(`username${userNumber}`, username.value);
+    localStorage.setItem(`password${userNumber}`, password.value);
+    localStorage.setItem(`role${userNumber}`, roleOption.value);
+    swal("Good Job", "Kamu sudah terdaftar cuy silahkan login!!!", "success");
+    masukRadio.click();
+  }
+};
 
-let signedInUser
-let signedInPassword
-let usernumber
-let role
+let usernumber;
 const signIn = () => {
-    if (search(username.value) && search(password.value)) {
-        signedInUser = `${username.value}del`
-        signedInPassword = `${password.value}del`
-        usernumber = searchKeys(username.value).split('username')[1]
-        role = localStorage.getItem(`role${usernumber}`)
-        roleStatus.innerHTML = `${role}`
-        window.alert(`Kamu sudah Masuk Cuy!!! Sebagai ${role}!!!`)
-        loginPage.style.visibility  = 'hidden'
-        dashboardPage.style.visibility = 'visible'
-    } else if (username.value === "" || password.value === "") {
-        window.alert("masukan username atau password")
-    } else if (!search(username.value) || !search(password.value)) {
-        window.alert('username atau password anda salah')
-    }
-}
-
+  if (username.value === "" || password.value === "") {
+    swal("Masukan Username Atau Password!!!");
+  } else if (!search(username.value) || !search(password.value)) {
+    swal("Username Atau Password Tidak Ditemukan!!!");
+  } else if (search(username.value) && search(password.value)) {
+    usernumber = searchKeys(username.value).split("username")[1];
+    let role = localStorage.getItem(`role${usernumber}`);
+    roleStatus.innerHTML = `${role}`;
+    usernameProfile.innerHTML = `${localStorage.getItem(`username${usernumber}`)}`;
+    loginPage.style.visibility = "hidden";
+    dashboardPage.style.visibility = "visible";
+    swal(`Kamu Sudah Masuk Cuy!!! Sebagai ${role}!!!`);
+    loginBox.reset();
+  }
+};
 
 const logOut = () => {
-    window.alert('Yakin Cuy Kamu Mau keluar ? (T_T)')
-    loginPage.style.visibility  = 'visible'
-    dashboardPage.style.visibility = 'hidden'
-    location.reload()
-}
+  swal("Yakin Cuy Kamu Mau keluar ? (T_T)").then((willLogOut) => {
+    if (willLogOut) {
+      loginPage.style.visibility = "visible";
+      dashboardPage.style.visibility = "hidden";
+    } else {
+      return;
+    }
+  });
+};
 
-const deleteAccount =() => {
-    window.alert('Yakin Cuy Akun Kamu Mau di Hapus ? (T_T)')
-    localStorage.removeItem(searchKeys(signedInUser.split("del")[0]))
-    localStorage.removeItem(searchKeys(signedInPassword.split("del")[0]))
-    location.reload()
-}
+const deleteAccount = () => {
+  swal({
+    title: "Are you sure?",
+    text: "Yakin Cuy Akun Kamu Mau di Hapus ? (T_T)",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  }).then((willDelete) => {
+    if (willDelete) {
+      swal("Akun Kamu Telah dihapus!", {
+        icon: "success",
+      });
+      console.log(usernumber);
+      localStorage.removeItem(`username${usernumber}`);
+      localStorage.removeItem(`password${usernumber}`);
+      localStorage.removeItem(`role${usernumber}`);
+      localStorage.removeItem(`images${usernumber}`);
+      loginPage.style.visibility = "visible";
+      dashboardPage.style.visibility = "hidden";
+    } else {
+      swal("OK, Akun Aman!");
+    }
+  });
+};
 
 //sidebar
 const sidebarToggler = () => {
-    sidebar.classList.toggle("sidebar-active2")
-}
+  sidebar.classList.toggle("sidebar-active2");
+};
 
+const resetApp = () => {
+  swal({
+    title: "Are you sure?",
+    text: "Aplikasi Akan di Reset, Semua Username dan Password Yang Tersimpan Akan Hilang. Yakin ?",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  }).then((willDelete) => {
+    if (willDelete) {
+      swal("Aplikasi telah di reset!", {
+        icon: "success",
+      });
+      localStorage.clear();
+      loginPage.style.visibility = "visible";
+      dashboardPage.style.visibility = "hidden";
+    } else {
+      swal("OK, Aplikasi Aman!");
+    }
+  });
+};
+
+//photo profile
+const ganti = () => {
+  const reader = new FileReader();
+
+  reader.addEventListener("load", () => {
+    localStorage.setItem(`images${usernumber}`, reader.result);
+  });
+
+  reader.readAsDataURL(inputImages.files[0]);
+  swal({
+    title: "Are you sure?",
+    text: "Photo Profile Akan Diganti...",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  }).then((willDelete) => {
+    if (willDelete) {
+      swal("Photo Profile Telah Diganti...", {
+        title: "Good Job!!",
+        icon: "success",
+      });
+      photoProfile.setAttribute("src", localStorage.getItem(`images${usernumber}`));
+    }
+  });
+};
+
+//Load Photo Profile
+signInButton.addEventListener("click", () => {
+  if (localStorage.getItem(`images${usernumber}`) === null) {
+    console.log(localStorage.getItem(`images${usernumber}`));
+    photoProfile.setAttribute("src", "./asset/kawaii.jpg");
+  } else {
+    photoProfile.setAttribute("src", localStorage.getItem(`images${usernumber}`));
+  }
+});
